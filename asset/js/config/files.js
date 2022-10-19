4239273cdd3abb76ac2382991e150a44aa79d3b9
장지훈 <me2designer@realsn.com>
2022-10-19 22:59:40 +0900

    reBuild 2022년 10월 19일 수 오후 10:59:39

--- asset/js/config/files.js
+++ asset/js/config/files.js
@@ -21,11 +21,9 @@ window.HOST = {
 window.SERVER = new Object();
 
 if (HOST.localhost || HOST.publish) { // 로컬 환경
-    SERVER.asset = "/asset";
-    SERVER.view = "/";
+    SERVER.asset = "/asset";    
 } else if (HOST.develop || HOST.product) { // 개발 및 운영서버
-    SERVER.asset = null;
-    SERVER.view = null;
+    SERVER.asset = null;    
 }
 /*
 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
