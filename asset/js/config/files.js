06d8a9b0f59beeb7ae732db3c771f6bfe1d5a5d5
장지훈 <me2designer@realsn.com>
2022-10-19 16:39:49 +0900

    reBuild 2022년 10월 19일 수 오후  4:39:49

---
+++ asset/js/config/files.js
@@ -0,0 +1,33 @@
+/*  cache */
+window.cache = "?v=" + new Date().getTime();
+
+/*
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+*/
+
+/* HOST */
+window.HOST = {
+    localhost: /\d+\.\d+\.\d+\.\d/.test(location.hostname) || /^localhost/.test(location.hostname) || /^design.devel.com/.test(location.hostname),    
+    publish: /^ux-mofa.realsn.com/.test(location.hostname) ,
+    develop: null,
+    product: null,
+};
+
+/*
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+*/
+
+/* resources path */
+window.SERVER = new Object();
+
+if (HOST.localhost || HOST.publish) { // 로컬환경
+    SERVER.asset = "/asset";
+    SERVER.view = "/";
+} else if (HOST.develop || HOST.product) { // 개발 및 운영서버
+    SERVER.asset = null;
+    SERVER.view = null;
+}
+
+/*
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+*/
