04bb9942faee6205800e61ba9a411db5a797a04b
장지훈 <me2designer@realsn.com>
2022-10-19 20:08:51 +0900

    reBuild 2022년 10월 19일 수 오후  8:08:50

--- asset/js/config/router.js
+++ asset/js/config/router.js
@@ -3,31 +3,27 @@
 const routes = [
   {
     path: "/login",
-    script: "/asset/js/pages/member.js",
+    script: `${SERVER.asset}/js/pages/member.js`,
   },
   {
     path: "/pw_set",
-    script: "/asset/js/pages/member.js",
-  },
-  {
-    path: "/member",
-    script: "/asset/js/pages/member.js",
+    script: `${SERVER.asset}/js/pages/member.js`,
   },
   {
     path: "/leaders",
-    script: "/asset/js/pages/leaders.js",
+    script: `${SERVER.asset}/js/pages/leaders.js`,
   },
   {
     path: "/media",
-    script: "/asset/js/pages/media.js",
+    script: `${SERVER.asset}/js/pages/media.js`,
   },
   {
     path: "/trend",
-    script: "/asset/js/pages/trend.js",
+    script: `${SERVER.asset}/js/pages/trend.js`,
   },
 ];
 
 const url = location.pathname.match(/(?<=\/\s*).*?(?=\s*\/)/gs);
-const hasPath = routes.find((_route) => _route.path.replace(/\//g, "") === url[url.length - 1])
+const hasPath = routes.find((_route) => _route.path.replace(/\//g, "") === url[url.length - 1]);
 
 if (hasPath) window.loadScript({ url: hasPath.script + window.cache });
