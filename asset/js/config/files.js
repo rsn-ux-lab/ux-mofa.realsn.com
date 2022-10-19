1cd60e1cf544a5e994d7bbb3e6dfcc27b80843a7
장지훈 <me2designer@realsn.com>
2022-10-19 22:48:05 +0900

    reBuild 2022년 10월 19일 수 오후 10:48:04

--- asset/js/config/files.js
+++ asset/js/config/files.js
@@ -9,8 +9,8 @@ window.cache = "?v=" + new Date().getTime();
 window.HOST = {
     localhost: /\d+\.\d+\.\d+\.\d/.test(location.hostname) || /^localhost/.test(location.hostname) || /^design.devel.com/.test(location.hostname),    
     publish: /^ux-mofa.realsn.com/.test(location.hostname) ,
-    develop: null,
-    product: null,
+    develop: /^mofa.realsn.com/.test(location.hostname),
+    product: /^mofa.realsn.com/.test(location.hostname),
 };
 
 /*
@@ -20,14 +20,13 @@ window.HOST = {
 /* resources path */
 window.SERVER = new Object();
 
-if (HOST.localhost || HOST.publish) { // 로컬환경
+if (HOST.localhost || HOST.publish) { // 로컬 환경
     SERVER.asset = "/asset";
     SERVER.view = "/";
 } else if (HOST.develop || HOST.product) { // 개발 및 운영서버
     SERVER.asset = null;
     SERVER.view = null;
 }
-
 /*
 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 */
