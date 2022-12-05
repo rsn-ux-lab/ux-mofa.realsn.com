82de0549731d99712dd1944c50da1d288a225d25
장지훈 <me2designer@realsn.com>
2022-12-06 02:15:37 +0900

    reBuild 2022년 12월  6일 화 오전  2:14:38

--- dashboard/asset/js/config/router.js
+++ dashboard/asset/js/config/router.js
@@ -2,23 +2,23 @@
 const routes = [
   {
     path: "/login",
-    script: `${SERVER.assets}/js/pages/login.js`,
+    script: `${SERVER.asset}/js/pages/login.js`,
   },
   {
     path: "/certify",
-    script: `${SERVER.assets}/js/pages/certify.js`,
+    script: `${SERVER.asset}/js/pages/certify.js`,
   },
   {
     path: "/leaders",
-    script: `${SERVER.assets}/js/pages/leaders.js`,
+    script: `${SERVER.asset}/js/pages/leaders.js`,
   },
   {
     path: "/media",
-    script: `${SERVER.assets}/js/pages/media.js`,
+    script: `${SERVER.asset}/js/pages/media.js`,
   },
   {
     path: "/trend",
-    script: `${SERVER.assets}/js/pages/trend.js`,
+    script: `${SERVER.asset}/js/pages/trend.js`,
   },
 ];
 
